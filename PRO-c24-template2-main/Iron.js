b9d6280e53fb6853335a0d8af38dcd5a6a783f41
rudra-op <82875311+rudra-op@users.noreply.github.com>
2021-05-14 15:18:44 +0530

    Add files via upload

---
+++ PRO-c24-template2-main/Iron.js
@@ -0,0 +1,30 @@
+class Iron{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.8,
+			friction:3,
+			density:30
+		}
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("brown");
+            rect(0,0,50,70)
+			pop()
+	}
+
+}
---
+++ PRO-c24-template2-main/sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(200,200,30,30)
+    stone = new Stone(400,400,40,40)
+    iron = new Iron(100,100,50,70)
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    stone.display();
+    iron.display();
+    
+
+    
+ 
+}
